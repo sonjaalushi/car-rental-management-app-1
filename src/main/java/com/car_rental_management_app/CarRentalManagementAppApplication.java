30c5563d657ec7401d62e7edb0ca46ece42a81d4
Alushi, Sonja <sonja.alushi@lhind.dlh.de>
2024-08-17 10:58:23 +0200

    Initial commit

---
+++ src/main/java/com/car_rental_management_app/CarRentalManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.car_rental_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/car_rental_management_app/CarRentalManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car_rental_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
